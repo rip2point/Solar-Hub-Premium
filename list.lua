b2ea52ddf4fb999d9cebc452c575ff74beb71282
xurel <93357496+rip2point@users.noreply.github.com>
2023-11-19 12:34:01 -0500

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,10 @@
+local Premium = "⭐"
+local Admin = "👑"
+
+local whitelisted = {
+	[759881255] = Admin,
+	[275263932] = Premium
+	--[123] = nil (can write anything here if they dont want their tag shown
+}
+
+return whitelisted
